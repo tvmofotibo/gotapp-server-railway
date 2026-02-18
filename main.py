26b805f11de6eef5cd138427e2b7e383e2d51e0a
Alvino Gabriel <73025025+tvmofotibo@users.noreply.github.com>
2026-02-17 22:43:33 -0300

    Change root endpoint to serve index.html

--- main.py
+++ main.py
@@ -348,4 +348,4 @@ async def send_message(message: MessageSend, current_user: User = Depends(get_cu
 
 @app.get("/")
 def root():
-    return {"message": "Got App API está rodando!"}
+    return FileResponse("static/index.html")
